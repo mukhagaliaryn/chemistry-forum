2730a86bddd9f916cfe93e2aeed4432357894160
mukhagaliaryn <mukhagaliaryn@gmail.com>
2023-02-04 16:36:59 +0600

    Done

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('table/', views.table, name='table'),
+    path('contact/', views.contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html', {})
+
+
+def about(request):
+    return render(request, 'main/about.html', {})
+
+
+def table(request):
+    return render(request, 'main/table.html', {})
+
+
+def contact(request):
+    return render(request, 'main/contact.html', {})
