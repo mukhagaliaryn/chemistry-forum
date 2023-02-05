7c17301c3937d1dd37202d0e4925fa7a403a83ff
mukhagaliaryn <mukhagaliaryn@gmail.com>
2023-02-05 21:59:58 +0600

    Done

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,16 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('table/', views.table, name='table'),
     path('contact/', views.contact, name='contact'),
+    path('human-body/', views.human_body, name='human_body'),
+
+    path('body/hair/', views.hair, name='hair'),
+    path('body/brain/', views.brain, name='brain'),
+    path('body/retina/', views.retina, name='retina'),
+    path('body/eye/', views.eye, name='eye'),
+    path('body/tooth/', views.tooth, name='tooth'),
+    path('body/heart/', views.heart, name='heart'),
+    path('body/blood/', views.blood, name='blood'),
+    path('body/kidney/', views.kidney, name='kidney'),
+    path('body/bones/', views.bones, name='bones'),
+    path('body/nails/', views.nails, name='nails'),
 ]
--- main/views.py
+++ main/views.py
@@ -15,3 +15,49 @@ def table(request):
 
 def contact(request):
     return render(request, 'main/contact.html', {})
+
+
+# Human body
+# ----------------------------------------------------------------------
+def human_body(request):
+    return render(request, 'main/body.html', {})
+
+
+def hair(request):
+    return render(request, 'main/body/hair.html', {})
+
+
+def brain(request):
+    return render(request, 'main/body/brain.html', {})
+
+
+def retina(request):
+    return render(request, 'main/body/retina.html', {})
+
+
+def eye(request):
+    return render(request, 'main/body/eye.html', {})
+
+
+def tooth(request):
+    return render(request, 'main/body/tooth.html', {})
+
+
+def heart(request):
+    return render(request, 'main/body/heart.html', {})
+
+
+def blood(request):
+    return render(request, 'main/body/blood.html', {})
+
+
+def kidney(request):
+    return render(request, 'main/body/kidney.html', {})
+
+
+def bones(request):
+    return render(request, 'main/body/bones.html', {})
+
+
+def nails(request):
+    return render(request, 'main/body/nails.html', {})
